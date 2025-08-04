1643b36a0b6cbcb1a6eba5afe6ec02366ff6c629
tonymcaron <tonymcaron@gmail.com>
2025-08-04 11:40:54 -0700

    Connecting DB to app, server-side validation and password hashing

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ Models = require('./models.js');
 
 const Movies = Models.Movie;
 const Users = Models.User;
-mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 const app = express();
 app.use(express.json());
