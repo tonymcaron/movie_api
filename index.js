c3adce4b10b8737ba5fb6724c32402db2f798bd7
tonymcaron <tonymcaron@gmail.com>
2025-08-08 10:12:27 -0700

    Slight format edits and updated a few URLs and query parameters

--- index.js
+++ index.js
@@ -267,3 +267,7 @@ const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
   console.log('Listening on Port ' + port);
 });
+
+// app.listen(8080, () => {
+  // console.log('Your app is listening on port 8080, boi!');
+// });
