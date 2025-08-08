fdf91941499efe8cfe1c7a3b80cd235aa9071267
tonymcaron <tonymcaron@gmail.com>
2025-08-08 13:32:10 -0700

    Formatted index.js and documentation.html

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors());
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
-const {check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 app.use(morgan('combined', { stream: accessLogStream }));
 app.use(express.static('public'));
@@ -34,7 +34,7 @@ app.get('/documentation', (req, res) => {
 });
 
 // List all movies
-app.get('/movies', passport.authenticate('jwt', {session: false}), async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.find()
     .then((movies) => {
       res.status(200).json(movies);
@@ -46,7 +46,7 @@ app.get('/movies', passport.authenticate('jwt', {session: false}), async (req, r
 });
 
 // Find movie details, by title
-app.get('/movies/:title', passport.authenticate('jwt', {session: false}), async (req, res) => {
+app.get('/movies/:title', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.findOne({ Title: req.params.title })
     .then((movie) => {
       res.json(movie);
@@ -58,7 +58,7 @@ app.get('/movies/:title', passport.authenticate('jwt', {session: false}), async 
 });
 
 // READ - Genre details, by genre name
-app.get('/movies/genre/:genreName', passport.authenticate('jwt', {session: false}), async (req, res) => {
+app.get('/movies/genre/:genreName', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.findOne({ 'Genre.Name': req.params.genreName })
     .then((genre) => {
       res.json(genre);
@@ -70,7 +70,7 @@ app.get('/movies/genre/:genreName', passport.authenticate('jwt', {session: false
 });
 
 // READ - Director details, by director name
-app.get('/movies/directors/:directorName', passport.authenticate('jwt', {session: false}), async (req, res) => {
+app.get('/movies/directors/:directorName', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.findOne({ 'Director.Name': req.params.directorName })
     .then((director) => {
       res.json(director);
@@ -92,7 +92,7 @@ Birthday: Date
 }*/
 app.post('/users',
   [
-    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username is required').isLength({ min: 5 }),
     check('Username', 'Username contains non-alphanumeric characters - not allowed.').isAlphanumeric(),
     check('Password', 'Password is required').not().isEmpty(),
     check('Email', 'Email does not appear to be valid').isEmail()
@@ -101,38 +101,38 @@ app.post('/users',
     // Check the validation object for errors
     let errors = validationResult(req);
 
-    if(!errors.isEmpty()) {
-      return res.status(422).json({errors: errors.array()});
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
 
-  let hashedPassword = Users.hashPassword(req.body.Password);
-  await Users.findOne({ Username: req.body.Username }) // Checks for whether or not username already exists
-    .then((user) => {
-      if (user) {
-        // If user is found, send a response stating so
-        return res.status(400).send(req.body.Username + ' already exists.');
-      } else {
-        Users.create({
-          Username: req.body.Username,
-          Password: hashedPassword,
-          Email: req.body.Email,
-          Birthday: req.body.Birthday
-        })
-          .then((user) => { res.status(201).json(user) })
-          .catch((error) => {
-            console.error(error);
-            res.status(500).send('Error: ' + error);
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    await Users.findOne({ Username: req.body.Username }) // Checks for whether or not username already exists
+      .then((user) => {
+        if (user) {
+          // If user is found, send a response stating so
+          return res.status(400).send(req.body.Username + ' already exists.');
+        } else {
+          Users.create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
           })
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
-    });
-});
+            .then((user) => { res.status(201).json(user) })
+            .catch((error) => {
+              console.error(error);
+              res.status(500).send('Error: ' + error);
+            })
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  });
 
 // Get all users
-app.get('/users', passport.authenticate('jwt', {session: false}), async (req, res) => {
+app.get('/users', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Users.find()
     .then((users) => {
       res.status(201).json(users);
@@ -144,8 +144,8 @@ app.get('/users', passport.authenticate('jwt', {session: false}), async (req, re
 });
 
 // Get user by username
-app.get('/users/:Username', passport.authenticate('jwt', {session: false}), async (req, res) => {
-  if(req.user.Username !== req.params.Username){
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  if (req.user.Username !== req.params.Username) {
     return res.status(400).send('Permission denied')
   }
   await Users.findOne({ Username: req.params.Username })
@@ -166,46 +166,46 @@ Password: String, (required),
 Email: String, (required),
 Birthday: Date
 }*/
-app.put('/users/:Username',  
-[
-  passport.authenticate('jwt', { session: false}),
-  check('Username', 'Username is required').isLength({min: 5}),
-  check('Username', 'Username contains non alphanumeric characters - not allowed.'),
-  check('Password', 'Password is required').not().isEmpty(),
-  check('Email', 'Email does not appear to be valid').isEmail()
-], async (req, res) => {
+app.put('/users/:Username',
+  [
+    passport.authenticate('jwt', { session: false }),
+    check('Username', 'Username is required').isLength({ min: 5 }),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.'),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], async (req, res) => {
 
-  // Check the validation object for errors
-  let errors = validationResult(req);
+    // Check the validation object for errors
+    let errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(422).json({errors: errors.array() });
-  }
-
-  if (req.user.Username !== req.params.Username) {
-    return res.status(400).send('Permission denied');
-  }
-  await Users.findOneAndUpdate({ Username: req.params.Username }, {
-    $set: {
-      Username: req.body.Username,
-      Password: req.body.Password,
-      Email: req.body.Email,
-      Birthday: req.body.Birthday
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-  },
-    { new: true }) //This line makes sure that the updated doc is returned
-    .then((updatedUser) => {
-      res.json(updatedUser);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    })
-});
+
+    if (req.user.Username !== req.params.Username) {
+      return res.status(400).send('Permission denied');
+    }
+    await Users.findOneAndUpdate({ Username: req.params.Username }, {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      }
+    },
+      { new: true }) //This line makes sure that the updated doc is returned
+      .then((updatedUser) => {
+        res.json(updatedUser);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      })
+  });
 
 // Add a movie to user's list of favs
-app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', {session: false}), async (req, res) => {
-  if(req.user.Username !== req.params.Username) {
+app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  if (req.user.Username !== req.params.Username) {
     return res.status(400).send('Permission denied');
   }
   await Users.findOneAndUpdate({ Username: req.params.Username }, {
@@ -222,8 +222,8 @@ app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', {sessi
 });
 
 // Delete a movie from user's fav list
-app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', {session: false}), async (req, res) => {
-  if(req.user.Username !== req.params.Username) {
+app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  if (req.user.Username !== req.params.Username) {
     return res.status(400).send('Permission denied');
   }
   await Users.findOneAndUpdate({ Username: req.params.Username }, {
@@ -240,8 +240,8 @@ app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', {ses
 });
 
 // Delete a user profile
-app.delete('/users/:Username', passport.authenticate('jwt', {session: false}), async (req, res) => {
-  if(req.user.Username !== req.params.Username) {
+app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  if (req.user.Username !== req.params.Username) {
     return res.status(400).send('Permission denied');
   }
   await Users.findOneAndDelete({ Username: req.params.Username })
@@ -264,10 +264,10 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0',() => {
+app.listen(port, '0.0.0.0', () => {
   console.log('Listening on Port ' + port);
 });
 
 // app.listen(8080, () => {
-  // console.log('Your app is listening on port 8080, boi!');
+// console.log('Your app is listening on port 8080, boi!');
 // });
