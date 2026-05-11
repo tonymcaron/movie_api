e5f64ff675fa137dde00103a8784af9a88c67620
tonymcaron <tonymcaron@gmail.com>
2026-05-11 14:58:42 -0700

    Changes for Vercel deploy

--- index.js
+++ index.js
@@ -5,8 +5,6 @@
 
 const express = require('express');
 morgan = require('morgan');
-fs = require('fs');
-path = require('path');
 mongoose = require('mongoose');
 Models = require('./models.js');
 
@@ -19,7 +17,7 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' });
+app.use(morgan('combined'));
 const cors = require('cors');
 app.use(cors());
 let auth = require('./auth')(app);
@@ -27,7 +25,6 @@ const passport = require('passport');
 require('./passport');
 const { check, validationResult } = require('express-validator');
 
-app.use(morgan('combined', { stream: accessLogStream }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -495,3 +492,5 @@ const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
   console.log('Listening on Port ' + port);
 });
+
+module.exports = app;
