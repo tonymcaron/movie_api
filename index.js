ccfd4c4ead3e821f3b3b67c6a510441d825af393
tonymcaron <tonymcaron@gmail.com>
2026-05-12 09:34:48 -0700

    Added missed comma

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const Users = Models.User;
 // mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
   serverSelectionTimeoutMS: 5000
 });
 
