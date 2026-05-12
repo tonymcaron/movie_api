094665e134111ccf5ecd360d8b63871e19d306af
tonymcaron <tonymcaron@gmail.com>
2026-05-12 11:17:16 -0700

    Add .then and .catch

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ const Movies = Models.Movie;
 const Users = Models.User;
 // mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// mongoose.connect(process.env.CONNECTION_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverSelectionTimeoutMS: 5000
-// });
+mongoose.connect(process.env.CONNECTION_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+  connectTimeoutMS: 5000,
+})
+  .them(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error("MongoDB connection error: ", err));
 
 const app = express();
 app.use(express.json());
